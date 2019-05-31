7ff8b50812e8123bc0a01b74ff944152756562f4
XRobquin <xrobquin@enseirb-matmeca.fr>
2019-05-31 23:30:28 +0200

    Update action-anniversaire.py

--- action-anniversaire.py
+++ action-anniversaire.py
@@ -18,24 +18,41 @@ def intent_received(hermes, intent_message):
 	print(intent_message.intent.intent_name)
 	print ()
 	
-	
-	janniv = 1
-	manniv = 11
-
-
-	now = datetime.datetime.now()
-	year = now.year
-	today = datetime.date.today()
-	anniv = datetime.date(year+1, manniv, janniv) 
-	diff = anniv - today
-
-
 	if intent_message.intent.intent_name == 'xrobquin:question_anniversaire':
 		
-		liste_reponses_marie = ["Il reste "+str(diff.days%365-1)+" jours avant votre anniversaire"]
-					    
+		if len(intent_message.slots.Name)==0:
+			if (name =='William'):
+				janniv = 2
+				manniv = 6
+			if (name =='Marie'):
+				janniv = 3
+				manniv = 6
+			
+				
+	
 		
-		sentence = liste_reponses_marie[random.randint(0,len(liste_reponses_marie)-1)]			
+		else:
+			janniv = 1
+			manniv = 11
+
+
+		now = datetime.datetime.now()
+		year = now.year
+		today = datetime.date.today()
+		anniv = datetime.date(year+1, manniv, janniv) 
+		diff = anniv - today
+
+		liste_reponses = ["Il reste "+str(diff.days%365-1)+" jours avant votre anniversaire"]			    
+
+		sentence = liste_reponses[random.randint(0,len(liste_reponses)-1)]		
+	
+			
+	
+	
+
+
+
+		
 				
 			
 		hermes.publish_end_session(intent_message.session_id, sentence)
