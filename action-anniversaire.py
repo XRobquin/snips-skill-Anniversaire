499ac13c31d3b629cf3926b424d9812d8135a790
XRobquin <xrobquin@enseirb-matmeca.fr>
2019-05-31 23:41:14 +0200

    Update action-anniversaire.py

--- action-anniversaire.py
+++ action-anniversaire.py
@@ -20,7 +20,8 @@ def intent_received(hermes, intent_message):
 	
 	if intent_message.intent.intent_name == 'xrobquin:question_anniversaire':
 		
-		if len(intent_message.slots.Name)==0:
+		if len(intent_message.slots.Name)==1:
+			name = intent_message.slots.Name.first().value	
 			if (name =='William'):
 				janniv = 2
 				manniv = 6
